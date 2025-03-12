701e3689806481984ca0e8d2382925eea26ce669
KoshTatus <153766064+KoshTatus@users.noreply.github.com>
2025-03-12 12:17:41 +0400

    fix: display message about no connection

--- app/src/main/java/com/example/weatherapp/ui/screens/WeatherScreen.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/WeatherScreen.kt
@@ -2,10 +2,8 @@ package com.example.weatherapp.ui.screens
 
 import android.annotation.SuppressLint
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,15 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.weatherapp.data.api.WeatherClient
-import com.example.weatherapp.data.models.City
-import com.example.weatherapp.ui.components.CityCard
 import com.example.weatherapp.ui.components.WeatherCard
 import com.example.weatherapp.ui.viewmodels.WeatherViewModel
 import java.text.SimpleDateFormat
@@ -48,24 +42,25 @@ fun WeatherScreen(
     weatherViewModel.fetchWeatherForCity(cityName)
     val weatherData by weatherViewModel.weatherData.collectAsState()
 
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 23.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = { navController.popBackStack() }) {
+            Icon(
+                Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Back"
+            )
+        }
+    }
+
     if (weatherData != null){
         val hourlyData = weatherData!!.hourly
         val times = hourlyData.time
         val temps = hourlyData.temperature2m
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 23.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            IconButton(onClick = { navController.popBackStack() }) {
-                Icon(
-                    Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "Back"
-                )
-            }
-        }
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -97,6 +92,20 @@ fun WeatherScreen(
             items(times.zip(temps)){ (time, temp) ->
                 WeatherCard(formatDateTime(time), temp)
             }
+        }
+    }
+    else{
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(65.dp),
+            horizontalArrangement = Arrangement.Center
+        ){
+            Text(
+                text = "Нет подключения к Интернету",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.displaySmall
+            )
         }
     }
 }
